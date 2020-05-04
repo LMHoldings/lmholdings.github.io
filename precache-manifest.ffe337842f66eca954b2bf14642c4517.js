cb877c4e7535bf72315dbc9c4b4e513c27e449ed
Luyolo <44893424+yolopalisson@users.noreply.github.com>
2020-05-04 21:36:25 +0200

    Create precache-manifest.ffe337842f66eca954b2bf14642c4517.js

---
+++ precache-manifest.ffe337842f66eca954b2bf14642c4517.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815d2321e28e14d8d193219411866fe1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4310f9ddf6776e032f00",
+    "url": "./static/css/main.08a4a1af.chunk.css"
+  },
+  {
+    "revision": "9f4c550cd6210ea2f7e2",
+    "url": "./static/js/2.670551b2.chunk.js"
+  },
+  {
+    "revision": "7cc9bc71dc193dc16a761d4db5c08eb5",
+    "url": "./static/js/2.670551b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4310f9ddf6776e032f00",
+    "url": "./static/js/main.5a3a67ea.chunk.js"
+  },
+  {
+    "revision": "00bdbf392ef36ae2a998",
+    "url": "./static/js/runtime-main.f91028b3.js"
+  },
+  {
+    "revision": "f23a5ac8c36d9e66968c04c5b1894ab2",
+    "url": "./static/media/Clearance.f23a5ac8.jpg"
+  },
+  {
+    "revision": "fb15f3e6d4b5413f5e78eca62165e6bd",
+    "url": "./static/media/LM Logo.fb15f3e6.jpg"
+  },
+  {
+    "revision": "d64a07942b715632bd545c2cdff84940",
+    "url": "./static/media/article1.d64a0794.jpg"
+  },
+  {
+    "revision": "46bd93f72e570bed9602a2296e24f5cd",
+    "url": "./static/media/article2.46bd93f7.jpg"
+  },
+  {
+    "revision": "4ffc04dc70aef68ca1328708c5b8d772",
+    "url": "./static/media/article3.4ffc04dc.jpg"
+  },
+  {
+    "revision": "2914d50d5d142ad7a93d34e6ca7723ae",
+    "url": "./static/media/bags open.2914d50d.jpg"
+  },
+  {
+    "revision": "19d048dc7081fa3166828ec62418689d",
+    "url": "./static/media/bags.19d048dc.jpg"
+  },
+  {
+    "revision": "55c738f9085642c2bbba88149fcd4f38",
+    "url": "./static/media/field.55c738f9.jpg"
+  },
+  {
+    "revision": "f23f83f603de17c46f07cc8187a55daa",
+    "url": "./static/media/timeline.f23f83f6.png"
+  }
+]);
