1fe2420ce33173f47569d074d1c3719cecdefffc
Luyolo <44893424+yolopalisson@users.noreply.github.com>
2020-05-13 21:52:17 +0200

    Create precache-manifest.5d4a78608ffe456e0c8b89a62bf50f44.js

---
+++ precache-manifest.5d4a78608ffe456e0c8b89a62bf50f44.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619a3eee6737a9b1a588d927cfc30a40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8203c0bfdc25fae94c55",
+    "url": "./static/css/main.ba5a5ff8.chunk.css"
+  },
+  {
+    "revision": "b6e5a120d9f98734e798",
+    "url": "./static/js/2.49837723.chunk.js"
+  },
+  {
+    "revision": "7cc9bc71dc193dc16a761d4db5c08eb5",
+    "url": "./static/js/2.49837723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8203c0bfdc25fae94c55",
+    "url": "./static/js/main.b752d2c8.chunk.js"
+  },
+  {
+    "revision": "00bdbf392ef36ae2a998",
+    "url": "./static/js/runtime-main.f91028b3.js"
+  },
+  {
+    "revision": "f23a5ac8c36d9e66968c04c5b1894ab2",
+    "url": "./static/media/Clearance.f23a5ac8.jpg"
+  },
+  {
+    "revision": "fb15f3e6d4b5413f5e78eca62165e6bd",
+    "url": "./static/media/LM Logo.fb15f3e6.jpg"
+  },
+  {
+    "revision": "d64a07942b715632bd545c2cdff84940",
+    "url": "./static/media/article1.d64a0794.jpg"
+  },
+  {
+    "revision": "46bd93f72e570bed9602a2296e24f5cd",
+    "url": "./static/media/article2.46bd93f7.jpg"
+  },
+  {
+    "revision": "4ffc04dc70aef68ca1328708c5b8d772",
+    "url": "./static/media/article3.4ffc04dc.jpg"
+  },
+  {
+    "revision": "2914d50d5d142ad7a93d34e6ca7723ae",
+    "url": "./static/media/bags open.2914d50d.jpg"
+  },
+  {
+    "revision": "19d048dc7081fa3166828ec62418689d",
+    "url": "./static/media/bags.19d048dc.jpg"
+  },
+  {
+    "revision": "55c738f9085642c2bbba88149fcd4f38",
+    "url": "./static/media/field.55c738f9.jpg"
+  },
+  {
+    "revision": "8daabeec02d54ba62fdd6a423424bbb2",
+    "url": "./static/media/fieldguy.8daabeec.jpg"
+  },
+  {
+    "revision": "bcd94bed1ef37f65f6333e1243c4db8d",
+    "url": "./static/media/grains.bcd94bed.jpg"
+  },
+  {
+    "revision": "da4453a1d137347ea8362a2ec59c4b86",
+    "url": "./static/media/group.da4453a1.jpg"
+  },
+  {
+    "revision": "6246b1266f0c2174d036c858346d53aa",
+    "url": "./static/media/miners.6246b126.jpg"
+  },
+  {
+    "revision": "13645adb6bb5bcadcbe6db6886613771",
+    "url": "./static/media/sorg.13645adb.jpg"
+  },
+  {
+    "revision": "f23f83f603de17c46f07cc8187a55daa",
+    "url": "./static/media/timeline.f23f83f6.png"
+  }
+]);
