183b8b1057700f96d758bf53d4bc874ebb6b0ef1
yolopalisson <luyolopaliso@gmail.com>
2020-06-14 02:39:52 +0200

    Create precache-manifest.d94fe1e6bd95efb5fdc5f316a0cb5daf.js

---
+++ precache-manifest.d94fe1e6bd95efb5fdc5f316a0cb5daf.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb88c8532868b4f393169fbe4beb7d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "757272e68795baaf2350",
+    "url": "./static/css/main.ba5a5ff8.chunk.css"
+  },
+  {
+    "revision": "a835a0bad729eac2e290",
+    "url": "./static/js/2.b04f56b7.chunk.js"
+  },
+  {
+    "revision": "7cc9bc71dc193dc16a761d4db5c08eb5",
+    "url": "./static/js/2.b04f56b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757272e68795baaf2350",
+    "url": "./static/js/main.215b77a2.chunk.js"
+  },
+  {
+    "revision": "00bdbf392ef36ae2a998",
+    "url": "./static/js/runtime-main.f91028b3.js"
+  },
+  {
+    "revision": "f23a5ac8c36d9e66968c04c5b1894ab2",
+    "url": "./static/media/Clearance.f23a5ac8.jpg"
+  },
+  {
+    "revision": "fb15f3e6d4b5413f5e78eca62165e6bd",
+    "url": "./static/media/LM Logo.fb15f3e6.jpg"
+  },
+  {
+    "revision": "d64a07942b715632bd545c2cdff84940",
+    "url": "./static/media/article1.d64a0794.jpg"
+  },
+  {
+    "revision": "46bd93f72e570bed9602a2296e24f5cd",
+    "url": "./static/media/article2.46bd93f7.jpg"
+  },
+  {
+    "revision": "4ffc04dc70aef68ca1328708c5b8d772",
+    "url": "./static/media/article3.4ffc04dc.jpg"
+  },
+  {
+    "revision": "2914d50d5d142ad7a93d34e6ca7723ae",
+    "url": "./static/media/bags open.2914d50d.jpg"
+  },
+  {
+    "revision": "19d048dc7081fa3166828ec62418689d",
+    "url": "./static/media/bags.19d048dc.jpg"
+  },
+  {
+    "revision": "55c738f9085642c2bbba88149fcd4f38",
+    "url": "./static/media/field.55c738f9.jpg"
+  },
+  {
+    "revision": "8daabeec02d54ba62fdd6a423424bbb2",
+    "url": "./static/media/fieldguy.8daabeec.jpg"
+  },
+  {
+    "revision": "bcd94bed1ef37f65f6333e1243c4db8d",
+    "url": "./static/media/grains.bcd94bed.jpg"
+  },
+  {
+    "revision": "da4453a1d137347ea8362a2ec59c4b86",
+    "url": "./static/media/group.da4453a1.jpg"
+  },
+  {
+    "revision": "b912e8b8d7b5be5b068cc4ecfc286450",
+    "url": "./static/media/group2.b912e8b8.jpg"
+  },
+  {
+    "revision": "11e6f3bd673c57b8dc2638bef40b4896",
+    "url": "./static/media/in the field.11e6f3bd.jpg"
+  },
+  {
+    "revision": "34a8e83e2b5391212282889c538adf19",
+    "url": "./static/media/man in the field.34a8e83e.jpg"
+  },
+  {
+    "revision": "3f7ed0b25f938a5cfdd234f22199f2a8",
+    "url": "./static/media/meeting.3f7ed0b2.jpg"
+  },
+  {
+    "revision": "6246b1266f0c2174d036c858346d53aa",
+    "url": "./static/media/miners.6246b126.jpg"
+  },
+  {
+    "revision": "13645adb6bb5bcadcbe6db6886613771",
+    "url": "./static/media/sorg.13645adb.jpg"
+  },
+  {
+    "revision": "fbcd16f65a2d7b69d45fedea3eba1c55",
+    "url": "./static/media/sorgfield.fbcd16f6.jpg"
+  },
+  {
+    "revision": "2a1f0a71e8ba0463d200441906611c07",
+    "url": "./static/media/sorgfield2.2a1f0a71.jpg"
+  },
+  {
+    "revision": "f23f83f603de17c46f07cc8187a55daa",
+    "url": "./static/media/timeline.f23f83f6.png"
+  }
+]);
